df416ce8a5286453913d35a6a4a7b223595acbf7
Grxmnir <Rafael>
2021-10-25 20:46:57 +0200

    ignore

--- Quiz App/script.js
+++ Quiz App/script.js
@@ -62,9 +62,9 @@ function loadQuiz(){
 
 }
 
-SubmitBtn.addEventListener(click, ()=>{
-    currentQuiz++;
-})
+SubmitBtn.addEventListener("click", () => {
+        currentQuiz++;
+    })
 
 
 loadQuiz();
